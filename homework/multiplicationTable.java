c12b902e0fe8f9f7156761eca9072702d5dca9f2
Kim Engse <error302@naver.com>
2024-02-23 13:37:44 +0900

    24.02.23 v6

--- homework/multiplicationTable.java
+++ homework/multiplicationTable.java
@@ -7,5 +7,11 @@ public class multiplicationTable {
 		for(int i = 1; i < 10; i++) System.out.println(number + " * " + i + " = " + i*number);
 		System.out.println("-".repeat(15));
 	}
-
+	
+	public void printHorizental() {
+		for(int i = 1; i < 10; i++) {
+			for(int j = 2; j < 10; j++) System.out.print(j + " * " + i + " = " + i*j + "\t");
+			System.out.println();
+		}
+	}
 }
--- homework/multiplicationTableTest.java
+++ homework/multiplicationTableTest.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class multiplicationTableTest {
 	public static void main(String[] args) {
 		multiplicationTable table = new multiplicationTable();
-		int num;
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			System.out.print("단 정보 입력 : ");
-			try {
-				num = sc.nextInt();
-			} catch (Exception e) {
-				System.out.println("잘못된 문자 입력 : " + e.getMessage());
-				break;
-			}
-			table.print(num);
-		}
+//		int num;
+//		Scanner sc = new Scanner(System.in);
+//		while (true) {
+//			System.out.print("단 정보 입력 : ");
+//			try {
+//				num = sc.nextInt();
+//			} catch (Exception e) {
+//				System.out.println("잘못된 문자 입력 : " + e.getMessage());
+//				break;
+//			}
+//			table.print(num);
+//		}
+		table.printHorizental();
 	}
 }
