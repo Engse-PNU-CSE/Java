a46b691f0a856f5d9b866f39cc65f082b54fba81
Kim Engse <error302@naver.com>
2024-02-29 16:52:33 +0900

    24.02.29 v4

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -8,11 +8,25 @@ public class Main {
 		String s = sc.next();
 		int n = sc.nextInt();
 		int num[] = new int[s.length()];
-		for(int i = 0; i < s.length(); i++) {
-			char c=s.charAt(i);
-			num[i]=(int)c - 55;
+		if(n > 10) { 
+			for(int i = 0; i < s.length(); i++) {
+				char c=s.charAt(i);
+				num[i]=(int)c - 55;
+			}
 		}
-		
+		else {
+			for(int i = 0; i < s.length(); i++) {
+				char c=s.charAt(i);
+				num[i]=(int)c - 48;
+			}
+		}
+		long tmp = 1;
+		long sum = 0;
+		for(int i = s.length()-1; i >= 0; i--) {
+			sum += num[i] * tmp;
+			tmp *= n;
+		}
+		System.out.println(sum);
 		sc.close();
 	}
 }
